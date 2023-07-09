e2baa7eb34179f839c37d13a9923a74c682fda20
edisonram <edisonlive2000@gmail.com>
2023-07-09 11:39:32 -0500

    reportes modificado

--- resources/views/reportes/reportes.blade.php
+++ resources/views/reportes/reportes.blade.php
@@ -79,25 +79,6 @@
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
     <script>
-    $('#libros').DataTable({
-        //"scrollY": "500px", // Altura máxima de la tabla
-        //"scrollCollapse": true,
-        //para cambiar el lenguaje a español
-        "language": {
-            "lengthMenu": "Mostrar _MENU_ registros",
-            "zeroRecords": "No se encontraron resultados",
-            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-            "sSearch": "Buscar:",
-            "oPaginate": {
-                "sFirst": "Primero",
-                "sLast": "Último",
-                "sNext": "Siguiente",
-                "sPrevious": "Anterior"
-            },
-            "sProcessing": "Procesando...",
-        }
-    });
+
     </script>
 @stop
