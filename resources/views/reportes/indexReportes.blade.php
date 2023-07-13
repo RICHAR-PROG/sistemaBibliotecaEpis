ebe4f1b4edd9335a1f15d0faf142eb38ebf00688
edisonram <edisonlive2000@gmail.com>
2023-07-13 11:35:32 -0500

    reports modifed

--- resources/views/reportes/indexReportes.blade.php
+++ resources/views/reportes/indexReportes.blade.php
@@ -2,8 +2,8 @@
 
 @section('title', 'Dashboard')
 @section('css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
 @endsection
 
 
@@ -11,112 +11,142 @@
 
     <body class="sb-nav-fixed">
 
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Reportes</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Estadisticas</li>
-                        </ol>
-                        {{-- <div class="card mb-4">
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Reportes</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Estadisticas</li>
+                    </ol>
+                    {{-- <div class="card mb-4">
                             <div class="card-body">Chart.js is a third party plugin that is used to generate the charts in this template. The charts below have been customized - for further customization options, please visit the official
                                 <a target="_blank" href="https://www.chartjs.org/docs/latest/">Chart.js documentation</a>.</div>
                         </div> --}}
-                        {{-- card informativo --}}
-                        <div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">
-                                        Total de Usuarios
-                                        <h2>{{ $user }}</h2>
-                                    </div>
-                                    <div class=" card-footer d-flex align-items-center justify-content-between">
-                                        <a Class="small-box-footer text-white stretched-link" href="{{route('users.index')}}">Ver Detalles</a>
-                                        <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
-                                    </div>
+                    {{-- card informativo --}}
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">
+                                    Total de Usuarios
+                                    <h2>{{ $user }}</h2>
                                 </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">
-                                        Total de Titulos
-                                        <h2>{{ $title }}</h2>
-                                    </div>
-                                    <div class=" card-footer d-flex align-items-center justify-content-between">
-                                        <a Class="small-box-footer text-white stretched-link" href="{{route('libros.index')}}">Ver Detalles</a>
-                                        <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">
-                                        Total de libros prestados
-                                        <h2>{{ $provided }}</h2>
-                                    </div>
-                                    <div class=" card-footer d-flex align-items-center justify-content-between">
-                                        <a Class="small-box-footer text-white stretched-link" href="#">Ver Detalles</a>
-                                        <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">
-                                        Total de libros devueltos
-                                        <h2>0</h2>
-                                    </div>
-                                    <div class=" card-footer d-flex align-items-center justify-content-between">
-                                        <a Class="small-box-footer text-white stretched-link" href="#">Ver Detalles</a>
-                                        <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
-                                    </div>
+                                <div class=" card-footer d-flex align-items-center justify-content-between">
+                                    <a Class="small-box-footer text-white stretched-link"
+                                        href="{{ route('users.index') }}">Ver Detalles</a>
+                                    <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
                                 </div>
                             </div>
                         </div>
-                        <div class="card mb-4">
-                            <div class="card-header"><i class="fas fa-chart-area mr-1"></i>Area Chart Example</div>
-                            <div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas>
-
-
-
-                            </div>
-                            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="card mb-4">
-                                    <div class="card-header"><i class="fas fa-chart-bar mr-1"></i>Bar Chart Example</div>
-                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="50"></canvas></div>
-                                    <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">
+                                    Total de Titulos
+                                    <h2>{{ $title }}</h2>
+                                </div>
+                                <div class=" card-footer d-flex align-items-center justify-content-between">
+                                    <a Class="small-box-footer text-white stretched-link"
+                                        href="{{ route('libros.index') }}">Ver Detalles</a>
+                                    <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
                                 </div>
                             </div>
-                            <div class="col-lg-6">
-                                <div class="card mb-4">
-                                    <div class="card-header"><i class="fas fa-chart-pie mr-1"></i>Pie Chart Example</div>
-                                    <div class="card-body"><canvas id="myPieChart" width="100%" height="50"></canvas></div>
-                                    <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">
+                                    Total de libros prestados
+                                    <h2>{{ $provided }}</h2>
+                                </div>
+                                <div class=" card-footer d-flex align-items-center justify-content-between">
+                                    <a Class="small-box-footer text-white stretched-link" href="#">Ver Detalles</a>
+                                    <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">
+                                    Total de libros devueltos
+                                    <h2>0</h2>
+                                </div>
+                                <div class=" card-footer d-flex align-items-center justify-content-between">
+                                    <a Class="small-box-footer text-white stretched-link" href="#">Ver Detalles</a>
+                                    <div class="small text-white"><i class=" fas fa-angle-right"></i></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 201</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
+                    <div class="card mb-4">
+                        <div class="card-header"><i class="fas fa-chart-area mr-1"></i>Area Chart Example</div>
+                        <div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas>
+                        </div>
+                        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                    </div>
+                    <div class="row">
+                        <!-- PIE CHART -->
+                        <div class="col-lg-6">
+                        <div class="card card-danger">
+                            <div class="card-header">
+                                <h3 class="card-title">Libros leidos</h3>
+
+                                <div class="card-tools">
+                                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                        <i class="fas fa-times"></i>
+                                    </button>
+                                </div>
                             </div>
+                            <div class="card-body">
+                                <canvas id="pieChart"
+                                    style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        </div>
+                        <!-- DONUT CHART -->
+                        <div class="col-lg-6">
+                        <div class="card card-success">
+                            <div class="card-header">
+                                <h3 class="card-title">Libros prestados</h3>
+
+                                <div class="card-tools">
+                                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                        <i class="fas fa-times"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <canvas id="donutChart"
+                                    style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
                         </div>
                     </div>
-                </footer>
-            </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <p class="text-center">©Copyright {{date('Y')}} EQUIPO 4. Todos los derechos reservados.</p>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+        </script>
         <script src="js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/chart-area-demo.js"></script>
@@ -124,14 +154,65 @@
         <script src="assets/demo/chart-pie-demo.js"></script>
 
 
-@stop
+    @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+    @section('css')
+        <link rel="stylesheet" href="/css/admin_custom.css">
+    @stop
 
-@section('js')
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+    @section('js')
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+        <script>
+            //-------------
+            //- DONUT CHART -
+            //-------------
+            // Get context with jQuery - using jQuery's .get() method.
+            var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+            var donutData = {
+                labels: [
+                    'Chrome',
+                    'IE',
+                    'FireFox',
+                    'Safari',
+                    'Opera',
+                    'Navigator',
+                ],
+                datasets: [{
+                    data: [700, 500, 400, 600, 300, 100],
+                    backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+                }]
+            }
+            var donutOptions = {
+                maintainAspectRatio: false,
+                responsive: true,
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            new Chart(donutChartCanvas, {
+                type: 'doughnut',
+                data: donutData,
+                options: donutOptions
+            })
+        </script>
+        <script>
+            //-------------
+            //- PIE CHART -
+            //-------------
+            // Get context with jQuery - using jQuery's .get() method.
+            var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+            var pieData = donutData;
+            var pieOptions = {
+                maintainAspectRatio: false,
+                responsive: true,
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            new Chart(pieChartCanvas, {
+                type: 'pie',
+                data: pieData,
+                options: pieOptions
+            })
+        </script>
 
-@stop
+    @stop
